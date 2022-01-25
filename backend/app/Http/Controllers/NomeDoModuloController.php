26a3bd303b595dce56db6c3c65a0f9d5f7fb2480
gPinheirop <gabrielpedreira_@hotmail.com>
2022-01-25 12:14:36 -0300

    criação dos projetos frontend e backend
    frontend: Reactjs;
    backend: Laravel.

---
+++ backend/app/Http/Controllers/NomeDoModuloController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NomeDoModulo;
+use App\Http\Requests\StoreNomeDoModuloRequest;
+use App\Http\Requests\UpdateNomeDoModuloRequest;
+
+class NomeDoModuloController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreNomeDoModuloRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreNomeDoModuloRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\NomeDoModulo  $nomeDoModulo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(NomeDoModulo $nomeDoModulo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\NomeDoModulo  $nomeDoModulo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(NomeDoModulo $nomeDoModulo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateNomeDoModuloRequest  $request
+     * @param  \App\Models\NomeDoModulo  $nomeDoModulo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateNomeDoModuloRequest $request, NomeDoModulo $nomeDoModulo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\NomeDoModulo  $nomeDoModulo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(NomeDoModulo $nomeDoModulo)
+    {
+        //
+    }
+}
